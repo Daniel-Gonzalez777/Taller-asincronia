60106b1227cc452b09c75520206ab8d85cd2e0a6
Dirison Estiven Rozo Hernández <dirirh@MacBook-Air-de-Dirison.local>
2025-05-20 06:51:16 -0500

    Subida segunda-parte

--- Punto1.js
+++ Punto1.js
@@ -6,3 +6,19 @@ async function crearArchivo(num) {
     const ruta = path.join(__dirname, 'archivos', `archivo_${num}.txt`);
     await fs.writeFile(ruta, texto);
 }
+
+async function crearArchivos(cantidad) {
+    const carpeta = path.join(__dirname, 'archivos');
+
+    await fs.mkdir(carpeta, { recursive: false });
+
+    const tareas = [];
+    for (let i = 1; i <= cantidad; i++) {
+        tareas.push(crearArchivo(i));
+    }
+
+    await Promise.all(tareas);
+    console.log(`✅ Se generaron ${cantidad} archivos.`);
+}
+
+crearArchivos(1000);
